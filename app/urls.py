9739a07ccf928264feb3332d9a6dfc1cfff25ac4
zengboi <mithileshgautam666@gmail.com>
2021-12-20 22:48:06 +0530

    Login/Logout Functionality added

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import TaskCreate, TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskCreate, TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', TaskList.as_view(), name='task-list'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
--- app/views.py
+++ app/views.py
@@ -1,36 +1,52 @@
 from django.db import models
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.contrib.auth.views import LoginView
 
 
 from .models import Task
 from django.urls import reverse_lazy
 
 # Create your views here.
+#Task List View
 class TaskList(ListView):  
     model = Task
     context_object_name = 'tasks'
 
+#Task Detail View
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task_detail'
     template_name = "app/task_detail.html"
 
+#ToDo Task Create View
 class TaskCreate(CreateView):
     model = Task
     # template_name = "app/task_form.html"
     fields = '__all__'
     success_url = reverse_lazy('task-list')
 
+#ToDo Task Update View
 class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('task-list')
-
+#ToDo Task Delete View
 class TaskDelete(DeleteView):
     model = Task
     context_object_name = 'tasks'
     success_url = reverse_lazy('task-list')
     template_name = "app/task_delete.html"
+
+#Todo Task Login View
+class CustomLoginView(LoginView):
+    template_name = 'app/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('task-list')
